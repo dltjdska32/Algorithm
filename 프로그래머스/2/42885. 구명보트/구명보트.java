7907aa4a274533bb5fa8bb4a8d1e9b2d3ecfeaad
dltjdska32 <128951288+dltjdska32@users.noreply.github.com>
2026-06-09 11:03:25 +0900

    [level 2] Title: 구명보트, Time: 10.69 ms, Memory: 59.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        
+        int s = 0;
+        int e = people.length - 1;
+        
+        while (s <= e){
+           
+            if(people[s] + people [e]<= limit){
+                s++;
+            }
+            
+            e--;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
